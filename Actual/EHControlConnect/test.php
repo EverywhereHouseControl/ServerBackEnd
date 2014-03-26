6681d58aeac7dd1e89ad04546af003477485b924
joseanrey <jose.an.rey@gmail.com>
2014-03-26 19:44:57 +0100

    test user

--- Actual/EHControlConnect/test.php
+++ Actual/EHControlConnect/test.php
@@ -8,10 +8,24 @@
 ?>
 <html>
 <body>
-  <form action="<?php $_PHP_SELF ?>" method="POST">
+  <form action="http://ehcontrol.net/EHControlConnect/index.php" method="POST">
 
-  Name: <input type="text" name="name" />
-  Age: <input type="text" name="age" />
+  Command: <input type="text" name="command" /></br>
+  Username: <input type="text" name="username" /></br>
+  Password: <input type="text" name="password" /></br>
+  n_username: <input type="text" name="n_username" /></br>
+  n_password: <input type="text" name="n_password" /></br>
+  n_email: <input type="text" name="n_email" /></br>
+  n_hint: <input type="text" name="n_hint" /></br>
+  house_name: <input type="text" name="house_name" /></br>
+  room_name: <input type="text" name="room_name" /></br>
+  service_name: <input type="text" name="service_name" /></br>
+  action_name: <input type="text" name="action_name" /></br>
+  data: <input type="text" name="data" /></br>
+  device: <input type="text" name="device" /></br>
+  ciudad: <input type="text" name="ciudad" /></br>
+  pais: <input type="text" name="pais" /></br>
+  idioma: <input type="text" name="idioma" /></br>
 
   <input type="submit" />
   </form>
